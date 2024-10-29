9e0568e6864faf381c5cc148d3aef602d25d4917
Amina Liaquat <aminaliaquat41@gmail.com>
2024-10-29 09:36:15 +0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,22 @@
+def CalculateGmean(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+def isGreater(a,b):
+    if(a>b):
+      print("First Number is Greater")
+    else:
+        print("Second Number is Greater")
+def isLesser(a,b):
+    pass
+a=5
+b=9
+isGreater(a,b)
+CalculateGmean(a,b)
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
+c=8
+d=4
+isGreater(c,d)
+CalculateGmean(c,d)
+# gmean2 = (c*d)/(c+d)
+# print(gmean2) 
