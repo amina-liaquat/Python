ecd2aa0cd6a095b6c7d8ea3618a24706530df3cf
Amina Liaquat <aminaliaquat41@gmail.com>
2025-07-22 10:03:22 +0500

    Create operators.py

---
+++ Basic Python/operators.py
@@ -0,0 +1,8 @@
+#Arthimetic Operator 
+print (5+7) #Addition
+print (5-7) #Subtraction
+print (5*7)  #Multiplication
+print (5/7)   #Dvision
+print (5%7)    # Modulus
+print (5//7)    #Floor Division
+print(5**7)  #Exponentiol
