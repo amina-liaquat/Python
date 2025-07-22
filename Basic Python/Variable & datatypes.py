e16afaceff1f438f3b6272a296d6fdffb14c5d10
Amina Liaquat <aminaliaquat41@gmail.com>
2025-07-22 09:55:49 +0500

    Create Variable & datatypes.py

---
+++ Basic Python/Variable & datatypes.py
@@ -0,0 +1,35 @@
+a= 4   #integer
+b= "Amina"  #string
+c=9.4      #float
+d=True      #boolean
+# Sequence data ; tuple ,list 
+list1= [8,2.3,[-4,5],["apple","banana"]]
+print(list1)
+tuple1=(("parrot","sparrow"),("Lion","Tiger"))
+print(tuple1)
+#Mapped data ; Dict
+dict1 = {"name" : "John", "age" : 36}
+print(dict1)
+
+   # TypeCasting
+
+# a=1
+# b=2
+# print(a+b)
+a = "1"
+b = "2"
+print(int(a)+int(b)) #Explict typecasting one datatype into other datatype by develpor
+c = 4.9
+d = 5
+print(c + d)   #Implict typecasting one datatype to another datatype by python
+
+    # Comments
+#single line comment
+# print("Hey My Name is Amina")
+# Multi line comment 
+'''
+Hello Hy Bye Bye
+bye bye ghjiwmw aksj
+'''
+# Escape Character
+print("Hey i am \"good boy\" \n and viewr is also good boy")
