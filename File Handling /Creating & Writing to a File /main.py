3c5dc2b161ed1fa8d6fa7d77bc67fad508cb80de
Amina Liaquat <aminaliaquat41@gmail.com>
2025-09-17 09:27:25 +0500

    Create main.py

---
+++ File Handling /Creating & Writing to a File /main.py
@@ -0,0 +1,8 @@
+# Open file in write mode
+file = open("example.txt", "w")
+
+file.write("Hello, this is my first file handling program!\n")
+file.write("Python makes it easy.\n")
+
+file.close()
+print("Data written successfully!")
