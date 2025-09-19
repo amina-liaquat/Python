efbbe5f438565b32dd21753094a2e39482ca79fd
Amina Liaquat <aminaliaquat41@gmail.com>
2025-09-19 10:08:32 +0500

    Create Handling specific exception.py

---
+++ Error & Exception Handling /Exception Handling /Handling specific exception.py
@@ -0,0 +1,7 @@
+try:
+    num = int(input("Enter a number: "))
+    result = 10 / num
+except ValueError:
+    print("That's not a valid number!")
+except ZeroDivisionError:
+    print("You can't divide by zero!")
