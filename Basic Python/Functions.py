b037a568bd2fbeb638aa2fbdbf6d9e9a6c988338
Amina Liaquat <aminaliaquat41@gmail.com>
2025-12-31 11:37:49 +0500

    Update Functions.py

--- Basic Python/Functions.py
+++ Basic Python/Functions.py
@@ -1,22 +1,29 @@
-def CalculateGmean(a,b):
-    mean=(a*b)/(a+b)
-    print(mean)
-def isGreater(a,b):
-    if(a>b):
-      print("First Number is Greater")
+import math
+
+def CalculateGmean(a, b):
+    gmean = math.sqrt(a * b)
+    print("Geometric Mean:", gmean)
+
+def isGreater(a, b):
+    if a > b:
+        print("First Number is Greater")
     else:
         print("Second Number is Greater")
-def isLesser(a,b):
-    pass
-a=5
-b=9
-isGreater(a,b)
-CalculateGmean(a,b)
-# gmean1 = (a*b)/(a+b)
-# print(gmean1)
-c=8
-d=4
-isGreater(c,d)
-CalculateGmean(c,d)
-# gmean2 = (c*d)/(c+d)
-# print(gmean2) 
+
+def isLesser(a, b):
+    if a < b:
+        print("First Number is Lesser")
+    else:
+        print("Second Number is Lesser")
+
+a = 5
+b = 9
+isGreater(a, b)
+isLesser(a, b)
+CalculateGmean(a, b)
+
+c = 8
+d = 4
+isGreater(c, d)
+isLesser(c, d)
+CalculateGmean(c, d)
