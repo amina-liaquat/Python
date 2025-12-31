603143eac6b7b32e43645a338ca89593c9a36118
Amina Liaquat <aminaliaquat41@gmail.com>
2025-12-31 11:41:34 +0500

    Update Tuples.py

--- Basic Python/Tuples.py
+++ Basic Python/Tuples.py
@@ -1,43 +1,42 @@
-tuple1=(1,2,3,4,5,6,7)
-tuple2=("Red","Green","Yellow")
-print(tuple1)
-print(tuple2)
-details=("Abhijeet",8,"Buddy",1234)
-print(details)
-# Tuples indexes
-country=("Pakistan","India","Italy","Dubai")
-print(country[1])      # Positive indexing
-print(country[-3])     # Negative Indexing 
-  # Check for item
-country=("Pakistan","India","Italy","Dubai")
+tuple1 = (1, 2, 3, 4, 5, 6, 7)
+tuple2 = ("Red", "Green", "Yellow")
+
+print("Tuple 1:", tuple1)
+print("Tuple 2:", tuple2)
+
+details = ("Abhijeet", 8, "Buddy", 1234)
+print("Details:", details)
+
+country = ("Pakistan", "India", "Italy", "Dubai")
+
+print("Positive Indexing:", country[1])
+print("Negative Indexing:", country[-3])
+
 if "Pakistan" in country:
-    print("YES This IS Present" )
+    print("Pakistan is present.")
 else:
-    print("No This is not present")
-    #!! !! !!! //// ///// !!!! !!!  !!
-country = ("Spain", "Italy", "India", "England", "Germany")
-if "Russia" in country:
+    print("Pakistan is not present.")
+
+country2 = ("Spain", "Italy", "India", "England", "Germany")
+
+if "Russia" in country2:
     print("Russia is present.")
 else:
     print("Russia is absent.")
-    # Range of index
-     #!! !! !!! //// ///// !!!! !!!  !!
-#  Syntax: Tuple[start : end : jumpIndex]
+
 animals = ("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow")
-print(animals[3:7])     #using positive indexes
-print(animals[-7:-2])   #using negative indexes
-           #!! !! !!! //// ///// !!!! !!!  !!
-animals = ("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow")
-print(animals[4:])      #using positive indexes
-print(animals[-4:])     #using negative indexes
-           #!! !! !!! //// ///// !!!! !!!  !!
-animals = ("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow")
-print(animals[:6])      #using positive indexes
-print(animals[:-3])     #using negative indexes
-           #!! !! !!! //// ///// !!!! !!!  !!
-animals = ("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow")
-print(animals[::2])     #using positive indexes
-print(animals[-8:-1:2]) #using negative indexes
-           #!! !! !!! //// ///// !!!! !!!  !!
-animals = ("cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow")
+
+print(animals[3:7])
+print(animals[-7:-2])
+
+print(animals[4:])
+print(animals[-4:])
+
+print(animals[:6])
+print(animals[:-3])
+
+print(animals[::2])
+print(animals[-8:-1:2])
+
 print(animals[1:8:3])
+
